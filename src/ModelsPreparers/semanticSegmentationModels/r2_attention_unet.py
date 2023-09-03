d1a8bd5e2750f914dba65a151881c8f51f1b8e15
hamzaraouzi <hamza.raouzi2016@gmail.com>
2023-09-03 23:34:44 +0100

    fix output dim for seg models

--- src/ModelsPreparers/semanticSegmentationModels/r2_attention_unet.py
+++ src/ModelsPreparers/semanticSegmentationModels/r2_attention_unet.py
@@ -95,9 +95,11 @@ class R2AttU_net(AbstrctSegmenter):
         d2 = torch.cat((x1, d2), dim=1)
         d2 = self.up_rrcnn2(d2)
 
+        d2 = self.conv_1x1(d2)
+
         if self.num_classes == 1:
             d2 = d2.squeeze(1)
-        return
+        return d2
 
     @staticmethod
     def prepareModel(
